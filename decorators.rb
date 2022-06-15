91a35b7efe67c8b76219eaa9b61505da25bc96e1
fed1k <allamurotovcoder@mail.ru>
2022-06-15 23:40:35 +0500

    Fix linters

--- decorators.rb
+++ decorators.rb
@@ -1,28 +1,26 @@
 require_relative 'nameable'
 require_relative 'person'
 class BaseDecorator
-    def initialize(nameable)
-        super()
-        @nameable = nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable
-    end
+  def correct_name
+    @nameable
+  end
 end
 
 class CapitalizeDecorator < BaseDecorator
-    def correct_name
-        return @nameable.correct_name.capitalize()
-    end
+  def correct_name
+    @nameable.correct_name.capitalize()
+  end
 end
 
 class TrimmerDecorator < BaseDecorator
-    def correct_name
-       if @nameable.correct_name.length > 10
-        puts @nameable.correct_name[0, 10]
-       end
-    end
+  def correct_name
+    @nameable.correct_name[0, 10] if @nameable.correct_name.length > 10
+  end
 end
 
 person = Person.new(22, 'maximilianus')
