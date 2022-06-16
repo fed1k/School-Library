8d34866883f214205a09e930427e43963bfd8040
fed1k <allamurotovcoder@mail.ru>
2022-06-16 18:47:46 +0500

    Fix linters part 2

--- student.rb
+++ student.rb
@@ -6,14 +6,10 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
-  end
-
-  def classroom(classroom)
-    @classroom = classroom
-    classroom.students << self unless classroom.students.include?(self)
   end
 end
