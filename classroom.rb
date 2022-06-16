d240de5362ad49f2258977c5177839cabdd63e54
fed1k <allamurotovcoder@mail.ru>
2022-06-16 06:57:40 +0500

    Fix linter issue with spacing

--- classroom.rb
+++ classroom.rb
@@ -1,5 +1,6 @@
 class Classroom
   attr_accessor :label, :students
+
   def initialize(label)
     @label = label
     @students = []
--- rental.rb
+++ rental.rb
@@ -2,6 +2,7 @@ require_relative 'book'
 require_relative 'person'
 class Rental
   attr_accessor :date, :book, :person
+
   def initialize(date, book, person)
     @date = date
     @book = book
